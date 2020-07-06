5ee061c52917ceef3ff51a04910f4818cdead418
YAben <abenov.erasyl@gmail.com>
2020-07-06 23:25:46 +0600

    feature TASKLIST - проверка добавления таблицу Priority

--- src/main/java/kz/app/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/kz/app/tasklist/backendspringboot/controller/PriorityController.java
@@ -2,6 +2,8 @@ package kz.app.tasklist.backendspringboot.controller;
 
 import kz.app.tasklist.backendspringboot.entity.Priority;
 import kz.app.tasklist.backendspringboot.repository.PriorityRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,7 +24,20 @@ public class PriorityController {
     }
 
     @PostMapping("/add")
-    public Priority add(@RequestBody Priority priority){
-        return priorityRepository.save(priority);
+    public ResponseEntity<Priority> add(@RequestBody Priority priority){
+
+        // проверка на объязательные параметры
+        if (priority.getId() != null && priority.getId() != 0) {
+            // id создается автоматически в БД (autoincrement), поетому его передавать не нужно,
+            // иначе может быть конфликт уникальности значения
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // если передали пустое значения title
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
     }
 }
